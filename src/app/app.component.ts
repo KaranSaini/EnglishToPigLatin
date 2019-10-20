d2bb19ffbd7de6e9dcbd73ef9794162abf0d8f84
Karan Saini <kss4382@gmail.com>
2019-10-20 00:05:49 -0400

    THIS PROJECT IS NOW WORKING AS INTENDED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'], 
+  animations: [                                     //will attempt angular animations here 
+    
+  ]
 })
 export class AppComponent {
+  bindedWord; 
+
+  pigLatinify(word) {
+    if(word.length < 3) {
+      return word; 
+    }
+    return word.slice(1) + '-' + word[0].toLowerCase() + 'ay'; 
+  }
+
+
+  onKey(typed) {
+    of(typed).pipe(
+      map(event => event.target.value), 
+      map(wordString => wordString.split(/\s+/)), 
+      map(wordArray => wordArray.map(this.pigLatinify))
+    ).subscribe(
+      translated => this.bindedWord = translated
+    );
   
+    this.bindedWord = this.bindedWord.join(" ");
+  }
+
 }
